751ee457c344a58595d3cb7c90cb9725df1590ac
Van Pho Nguyen <vanpho02@gmail.com>
2017-07-17 21:25:35 +0700

    xong

--- f.js
+++ f.js
@@ -1,6 +1,7 @@
-
 let str = '';
 
 for(let i = 1; i <= 5; i++) {
     console.log(str += i);
 }
+
+
---
+++ i.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+    console.log(a + b);
+}
+
+add(4, 5);
+add(5, 6);
+add(7, 15);
+
+function myAbs(num) {
+    console.log(num > 0 ? num : -num);
+}
+
+myAbs(10);
+myAbs(-10);
